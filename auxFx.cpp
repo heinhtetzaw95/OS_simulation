c0f1c0ab8464cc54e0dbbed41242ef6887f10e1a
FrancescoSTL <francescosapolizzi@gmail.com>
2016-04-26 19:09:40 -0500

    Add other files.

---
+++ auxFx.cpp
@@ -0,0 +1,25 @@
+/*
+ *	File Name				:	simulation_header.h
+ *	Primary Author			:
+ *	Contributing Author(s)	:
+ *	Date Created			:	26 April 2016
+ *	Date Last Modified		:	26 April 2016
+ *
+ *	Description		:	This is the driver for our OS Simulation where all
+ *						auxiliary functions are called
+ *
+ */
+
+	//libraries to include
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include "simulation_header.h"
+using namespace std;
+
+int main() {
+
+	cout << "hey, everything works so far" << endl;
+
+	return 0;
+}
